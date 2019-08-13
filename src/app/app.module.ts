57f515603248d3776b22a72637c1035925b5b7fa
Akhilesh <AkhileshM@outlook.in>
2019-08-13 20:25:52 +0530

    Added route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Route } from '@angular/router';
+
+import { McBreadcrumbsModule } from 'ngx-breadcrumbs';
 
 import { AppComponent } from './app.component';
+import { Route1Component } from './route1/route1.component';
+
+const routes: Route = [];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, Route1Component],
+  imports: [BrowserModule, RouterModule.forRoot(routes), McBreadcrumbsModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
