11764b2a9ead779667af89c473e06c6383ecc8cb
Akhilesh <AkhileshM@outlook.in>
2019-08-14 14:22:35 +0530

    Added dummy routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,31 @@ import { McBreadcrumbsModule } from 'ngx-breadcrumbs';
 import { AppComponent } from './app.component';
 import { Route1Component } from './route1/route1.component';
 
-const routes: Route = [];
+const routes: Route[] = [
+  {
+    path: '',
+    component: Route1Component,
+    data: {
+      breadcrumbs: 'Route 1'
+    }
+  },
+  {
+    path: 'route1',
+    component: Route1Component,
+    data: {
+      breadcrumbs: 'Route 1 1'
+    },
+    children: [
+      {
+        path: 'r2',
+        component: Route1Component,
+        data: {
+          breadcrumbs: 'Sub route'
+        }
+      }
+    ]
+  }
+];
 
 @NgModule({
   declarations: [AppComponent, Route1Component],
